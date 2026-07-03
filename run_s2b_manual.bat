660615a5fac2eaad371bac4ba1648150b73bfce9
Yeonjun Choe <choi2han124@gmail.com>
2026-07-04 01:57:53 +0900

    chore: default Windows runner to full run

--- run_s2b_manual.bat
+++ run_s2b_manual.bat
@@ -1,15 +1,15 @@
 @echo off
 setlocal
 if "%~1"=="" (
-  echo Usage: run_s2b_manual.bat "GOOGLE_SHEET_URL" [personal^|school] [dry^|run]
-  echo Example: run_s2b_manual.bat "https://docs.google.com/spreadsheets/d/.../edit?gid=0#gid=0" personal dry
+  echo Usage: run_s2b_manual.bat "GOOGLE_SHEET_URL" [personal^|school] [run^|dry]
+  echo Example: run_s2b_manual.bat "https://docs.google.com/spreadsheets/d/.../edit?gid=0#gid=0" personal run
   exit /b 2
 )
 set SHEET_URL=%~1
 set ACCOUNT=%~2
 set MODE=%~3
 if "%ACCOUNT%"=="" set ACCOUNT=personal
-if "%MODE%"=="" set MODE=dry
+if "%MODE%"=="" set MODE=run
 
 if exist .venv\Scripts\python.exe (
   .venv\Scripts\python.exe s2b_auto.py --sheet "%SHEET_URL%" --account %ACCOUNT% --mode %MODE% --manual-login-then-headless
